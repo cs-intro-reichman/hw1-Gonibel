3fc4b0d97de298850348e61b1d4e9fcb5692dc69
Gonibel <gbelkine@gmail.com>
2024-11-13 14:16:58 +0200

    HW 1

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,17 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		String bill = args[3];
+
+		double bill1 = Double.parseDouble(bill);
+
+		double split = bill1/3.0;
+		double split1 = Math.ceil(split);
+
+		System.out.println("Dear " + name1 + ", " + name2 + ", " + name3 + ". You have to pay " + split1 + " $ please.");
+
 	    // Replace this comment with the rest of your code   
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,25 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	 
+		// Replace this comment with your code
+		String num1 = args[0];
+
+		int num = Integer.parseInt(num1);
+
+		int hundreds = num / 100;
+		num = num % 100;
+		int tens = num / 10;
+		num = num % 10;
+
+		System.out.println(hundreds + " hundreds " + tens + " tens " + num + " ones");
+
+
+
+	
+	
+	
+	
+	
 	}
 }
